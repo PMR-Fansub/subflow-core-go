e516894d2b51639c0f27d549fc4a002d3df0f8e6
Ginakira <ginakira@foxmail.com>
2023-08-31 01:11:37 +0800

    refactor: make jwt middleware injected by fx

--- internal/api/v1/router.go
+++ internal/api/v1/router.go
@@ -6,56 +6,50 @@ import (
 	casbinware "github.com/gofiber/contrib/casbin"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
-	jwtware "github.com/gofiber/jwt/v2"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	fiberSwagger "github.com/swaggo/fiber-swagger"
-	"subflow-core-go/internal/api/common"
-	"subflow-core-go/internal/api/constants"
-	"subflow-core-go/internal/api/helper"
-	"subflow-core-go/internal/api/v1/handler"
-	"subflow-core-go/internal/config"
-
 	_ "subflow-core-go/docs"
+	"subflow-core-go/internal/api/constants"
+	"subflow-core-go/internal/api/v1/handler"
 )
 
 type Router struct {
 	router           fiber.Router
 	handler          *handler.Handler
 	casbinMiddleware *casbinware.Middleware
+	jwtMiddleware    fiber.Handler
 }
 
-var jwtMiddleware fiber.Handler
-
-func jwtErrorHandler(ctx *fiber.Ctx, err error) error {
-	result := common.ResultUnauthorized
-	resp := common.MakeAPIResponse(result, err.Error())
-	return ctx.Status(result.HttpCode).JSON(resp)
-}
-
-func NewRouter(app *fiber.App, handlerV1 *handler.Handler, cfg *config.Config, cm *casbinware.Middleware) *Router {
-	jwtMiddleware = jwtware.New(
-		jwtware.Config{
-			SigningMethod: "HS256",
-			SigningKey:    []byte(cfg.Server.SigningKey),
-			ErrorHandler:  jwtErrorHandler,
-			TokenLookup:   "cookie:auth_token,header:Authorization",
-			Claims:        &helper.UserClaim{},
-		},
-	)
+func NewRouter(app *fiber.App, handlerV1 *handler.Handler, cm *casbinware.Middleware, jm fiber.Handler) *Router {
 	return &Router{
 		router:           app.Group("v1"),
 		handler:          handlerV1,
 		casbinMiddleware: cm,
+		jwtMiddleware:    jm,
 	}
 }
 func (r *Router) Setup() {
-	r.router.Get("healthz", health)
-	r.router.Get("metrics", adaptor.HTTPHandler(promhttp.Handler()))
-	r.router.Get("/swagger/*", fiberSwagger.WrapHandler)
+	r.SetupHealthCheck()
+	r.SetupMetrics()
+	r.SetupSwagger()
 
 	r.SetupAuth()
 	r.SetupUser()
 	r.SetupTeam()
+	r.SetupWorkFlow()
+	r.SetupTask()
+}
+
+func (r *Router) SetupSwagger() fiber.Router {
+	return r.router.Get("/swagger/*", fiberSwagger.WrapHandler)
+}
+
+func (r *Router) SetupMetrics() fiber.Router {
+	return r.router.Get("metrics", adaptor.HTTPHandler(promhttp.Handler()))
+}
+
+func (r *Router) SetupHealthCheck() fiber.Router {
+	return r.router.Get("healthz", health)
 }
 
 func (r *Router) SetupAuth() {
@@ -69,7 +63,7 @@ func (r *Router) SetupUser() {
 	userGrp.Get("/:id", handler.WrapHandlerWithAutoParse(r.handler.GetUserByID))
 	userGrp.Get("/:id/teams", handler.WrapHandlerWithAutoParse(r.handler.GetUserTeamsByID))
 
-	userGrpWithAuth := r.router.Group("user", jwtMiddleware)
+	userGrpWithAuth := r.router.Group("user", r.jwtMiddleware)
 	userGrpWithAuth.Get("/", handler.WrapHandlerWithAutoParse(r.handler.GetCurrentUser))
 	userGrpWithAuth.Patch("/", handler.WrapHandlerWithAutoParse(r.handler.UpdateCurrentUser))
 	userGrpWithAuth.Patch(
@@ -84,6 +78,14 @@ func (r *Router) SetupUser() {
 
 func (r *Router) SetupTeam() {
 	// teamGrp := r.router.Group("team")
+}
+
+func (r *Router) SetupWorkFlow() {
+
+}
+
+func (r *Router) SetupTask() {
+
 }
 
 func health(c *fiber.Ctx) error {
